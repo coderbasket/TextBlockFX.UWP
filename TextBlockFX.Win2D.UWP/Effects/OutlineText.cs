488a66deabedbce46f5eb87876f3c528c83d7f62
Coder Basket <coderbasketcontact@gmail.com>
2022-10-28 13:32:02 +0800

    ok

--- TextBlockFX.Win2D.UWP/Effects/OutlineText.cs
+++ TextBlockFX.Win2D.UWP/Effects/OutlineText.cs
@@ -66,11 +66,7 @@ namespace TextBlockFX.Win2D.UWP.Effects
                state,
                drawingSession,
                args);
-            var ds = args.DrawingSession;
-            
-            textLayout = newTextLayout;
-            _textFormat = textFormat;
-            testString = newText;
+            var ds = args.DrawingSession;                      
             if (state == RedrawState.Idle)
             {
 
@@ -80,10 +76,8 @@ namespace TextBlockFX.Win2D.UWP.Effects
             Canvas_Draw(args.DrawingSession, newTextLayout.RequestedSize, args);
         }
         //Engine
-        string testString = "";
-        CanvasTextLayout textLayout;
+
         CanvasGeometry textGeometry;
-        CanvasTextFormat _textFormat;
         CanvasStrokeStyle dashedStroke = new CanvasStrokeStyle()
         {
             DashStyle = CanvasDashStyle.Dash
@@ -258,24 +252,17 @@ namespace TextBlockFX.Win2D.UWP.Effects
             if (!needsResourceRecreation)
                 return;
 
-            if (textLayout != null)
-            {
-                //textLayout.Dispose();
-                //if(textGeometry!= null)
-                //  textGeometry.Dispose();
-            }
-
             ApplyToTextLayout(resourceCreator, (float)targetSize.Width, (float)targetSize.Height);
 
             if (CurrentTextOutlineGranularityOption == TextOutlineGranularity.Layout)
             {
-                textGeometry = CanvasGeometry.CreateText(textLayout);
+                textGeometry = CanvasGeometry.CreateText(this.EffectParam.NewTextLayout);
             }
             else
             {
                 GlyphRunsToGeometryConverter converter = new GlyphRunsToGeometryConverter(resourceCreator);
 
-                textLayout.DrawToTextRenderer(converter, 0, 0);
+                this.EffectParam.NewTextLayout.DrawToTextRenderer(converter, 0, 0);
 
                 textGeometry = converter.GetGeometry();
             }
@@ -286,7 +273,7 @@ namespace TextBlockFX.Win2D.UWP.Effects
         private void ApplyToTextLayout(ICanvasResourceCreator resourceCreator, float canvasWidth, float canvasHeight)
         {
             
-            everyOtherWordBoundary = Utils.GetEveryOtherWord(testString);
+            everyOtherWordBoundary = Utils.GetEveryOtherWord(this.EffectParam.NewText);
 
             float sizeDim = Math.Min(canvasWidth, canvasHeight);
 
@@ -295,21 +282,21 @@ namespace TextBlockFX.Win2D.UWP.Effects
             {
                 foreach (var wb in everyOtherWordBoundary)
                 {
-                    textLayout.SetUnderline(wb.Start, wb.Length, true);
+                    this.EffectParam.NewTextLayout.SetUnderline(wb.Start, wb.Length, true);
                 }
             }
             else if (CurrentTextEffectOption == TextEffectOption.StrikeEveryOtherWord)
             {
                 foreach (var wb in everyOtherWordBoundary)
                 {
-                    textLayout.SetStrikethrough(wb.Start, wb.Length, true);
+                    this.EffectParam.NewTextLayout.SetStrikethrough(wb.Start, wb.Length, true);
                 }
             }
 
-            textLayout.TrimmingGranularity = CanvasTextTrimmingGranularity.Character;
-            textLayout.TrimmingSign = CanvasTrimmingSign.Ellipsis;
+            this.EffectParam.NewTextLayout.TrimmingGranularity = CanvasTextTrimmingGranularity.Character;
+            this.EffectParam.NewTextLayout.TrimmingSign = CanvasTrimmingSign.Ellipsis;
 
-            textLayout.VerticalGlyphOrientation = CurrentVerticalGlyphOrientation;
+            this.EffectParam.NewTextLayout.VerticalGlyphOrientation = CurrentVerticalGlyphOrientation;
 
             
         }
@@ -326,121 +313,10 @@ namespace TextBlockFX.Win2D.UWP.Effects
             {
                 Color semitrans = Colors.CornflowerBlue;
                 semitrans.A = 127;
-                args.DrawingSession.DrawTextLayout(textLayout, 0, 0, semitrans);
+                args.DrawingSession.DrawTextLayout(this.EffectParam.NewTextLayout, 0, 0, semitrans);
             }
         }
         
     }
-    static class Utils
-    {
-        public static List<T> GetEnumAsList<T>()
-        {
-            return Enum.GetValues(typeof(T)).Cast<T>().ToList();
-        }
-
-        public static Matrix3x2 GetDisplayTransform(Vector2 outputSize, Vector2 sourceSize)
-        {
-            // Scale the display to fill the control.
-            var scale = outputSize / sourceSize;
-            var offset = Vector2.Zero;
-
-            // Letterbox or pillarbox to preserve aspect ratio.
-            if (scale.X > scale.Y)
-            {
-                scale.X = scale.Y;
-                offset.X = (outputSize.X - sourceSize.X * scale.X) / 2;
-            }
-            else
-            {
-                scale.Y = scale.X;
-                offset.Y = (outputSize.Y - sourceSize.Y * scale.Y) / 2;
-            }
-
-            return Matrix3x2.CreateScale(scale) *
-                   Matrix3x2.CreateTranslation(offset);
-        }
-
-        public static CanvasGeometry CreateStarGeometry(ICanvasResourceCreator resourceCreator, float scale, Vector2 center)
-        {
-            Vector2[] points =
-            {
-                new Vector2(-0.24f, -0.24f),
-                new Vector2(0, -1),
-                new Vector2(0.24f, -0.24f),
-                new Vector2(1, -0.2f),
-                new Vector2(0.4f, 0.2f),
-                new Vector2(0.6f, 1),
-                new Vector2(0, 0.56f),
-                new Vector2(-0.6f, 1),
-                new Vector2(-0.4f, 0.2f),
-                new Vector2(-1, -0.2f),
-            };
-
-            var transformedPoints = from point in points
-                                    select point * scale + center;
-
-            return CanvasGeometry.CreatePolygon(resourceCreator, transformedPoints.ToArray());
-        }
-
-        public static float DegreesToRadians(float angle)
-        {
-            return angle * (float)Math.PI / 180;
-        }
-
-        static readonly Random random = new Random();
-
-        public static Random Random
-        {
-            get { return random; }
-        }
-
-        public static float RandomBetween(float min, float max)
-        {
-            return min + (float)random.NextDouble() * (max - min);
-        }
-
-        public static async Task<byte[]> ReadAllBytes(string filename)
-        {
-            var uri = new Uri("ms-appx:///" + filename);
-            var file = await StorageFile.GetFileFromApplicationUriAsync(uri);
-            var buffer = await FileIO.ReadBufferAsync(file);
-
-            return buffer.ToArray();
-        }
-
-        public static async Task<T> TimeoutAfter<T>(this Task<T> task, TimeSpan timeout)
-        {
-            if (task == await Task.WhenAny(task, Task.Delay(timeout)))
-            {
-                return await task;
-            }
-            else
-            {
-                throw new TimeoutException();
-            }
-        }
-
-        public struct WordBoundary { public int Start; public int Length; }
-
-        public static List<WordBoundary> GetEveryOtherWord(string str)
-        {
-            List<WordBoundary> result = new List<WordBoundary>();
-
-            for (int i = 0; i < str.Length; ++i)
-            {
-                if (str[i] == ' ')
-                {
-                    int nextSpace = str.IndexOf(' ', i + 1);
-                    int limit = nextSpace == -1 ? str.Length : nextSpace;
-
-                    WordBoundary wb = new WordBoundary();
-                    wb.Start = i + 1;
-                    wb.Length = limit - i - 1;
-                    result.Add(wb);
-                    i = limit;
-                }
-            }
-            return result;
-        }
-    }
+    
 }
